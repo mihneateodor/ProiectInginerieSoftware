3334a0ec74555f8b4ae2e55cbf2aeefa39bdaa07
gorganraul23 <gorganraul@yahoo.com>
2021-12-24 11:54:21 +0200

    toate repository_2

---
+++ proiect/src/main/java/com/proiect/proiect/model/PersoanaRepository.java
@@ -0,0 +1,7 @@
+package com.proiect.proiect.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersoanaRepository extends CrudRepository<Persoana, Integer> {
+
+}
---
+++ proiect/src/main/java/com/proiect/proiect/model/ZborRepository.java
@@ -0,0 +1,7 @@
+package com.proiect.proiect.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZborRepository extends CrudRepository<Zbor, Integer> {
+
+}
