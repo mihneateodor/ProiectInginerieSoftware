6c82b208796a8aace699bb1ba37ffe19d8dea972
CociubeiMihai <73403886+CociubeiMihai@users.noreply.github.com>
2021-12-28 19:38:34 +0200

    Mihai v2.01

--- proiect/src/main/java/com/proiect/proiect/ProiectController.java
+++ proiect/src/main/java/com/proiect/proiect/ProiectController.java
@@ -3,26 +3,18 @@ package com.proiect.proiect;
 
 import com.proiect.proiect.model.*;
 import com.proiect.proiect.repositories.AeroportRepository;
-import com.proiect.proiect.repositories.PersoanaRepository;
 import com.proiect.proiect.repositories.ZborRepository;
-import com.proiect.proiect.model.Aeroport;
-import com.proiect.proiect.model.AeroportRepository;
-import com.proiect.proiect.model.Persoana;
-import com.proiect.proiect.model.PersoanaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.proiect.proiect.model.PersoanaRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
